bb262bd2191a7ed7fce61097d30ed4f85b6c61ac
LeonelCedillo <leonel.cs.979@gmail.com>
2025-11-15 16:50:05 -0800

    add commands.ts and users.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,32 @@
-import { setUser, readConfig } from "./config";
+import { type CommandsRegistry, registerCommand, runCommand } from "./commands/commands";
+import { handlerLogin } from "./users";
+import { readConfig } from "./config";
 
 function main() {
-  setUser("Jeff");
-  const cfg = readConfig();
-  console.log(cfg);
+  const args = process.argv.slice(2);
+  if (args.length < 1) {
+    console.log("usage: cli <command> [args...]");
+    process.exit(1);
+  }
+
+  const cmdName = args[0];
+  const cmdArgs = args.slice(1);
+  const commandsRegistry: CommandsRegistry = {}
+
+  registerCommand(commandsRegistry, "login", handlerLogin);
+
+  try {
+    runCommand(commandsRegistry, cmdName, ...cmdArgs);
+    const cfg = readConfig();
+    console.log(cfg);
+  } catch (err) {
+    if (err instanceof Error) {
+      console.error(`Error running command ${cmdName}: ${err.message}`);
+    } else {
+      console.error(`Error running command ${cmdName}: ${err}`);
+    }
+    process.exit(1);
+  }
 }
 
 main();
